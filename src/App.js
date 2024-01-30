43200c66e7619684d82bbedf0dc9405c861bca99
Dario127 <vikashchauhan127@gmail.com>
2024-01-30 17:21:18 +0530

    adding a react project

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Done with react application!!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Thanks a lot!!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
